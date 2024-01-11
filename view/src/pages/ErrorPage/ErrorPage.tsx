5672b34a125d9af90b56724310f5b7f5b405667e
Joe Griffin <jgriffin1410@gmail.com>
2024-01-11 19:41:08 +0000

    updating untracked files

--- view/src/pages/ErrorPage/ErrorPage.tsx
+++ view/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,25 +1,20 @@
-import { Link as ReactRouterLink, isRouteErrorResponse, useRouteError } from 'react-router-dom';
-import { Box, Heading, Text, Link } from '@chakra-ui/react';
-import theme from '../../theme';
+import { isRouteErrorResponse, useRouteError } from "react-router-dom"
+import { Box, Heading, Text } from "@chakra-ui/react"
 
 const ErrorPage = () => {
     const error = useRouteError();
     console.log(error);
 
-    return (
-        // TODO: add styles
-        <Box padding="50" my="50%">
-            <Heading mx="auto" textAlign="center">
-                Oops!
-            </Heading>
-            <Text>
-                {isRouteErrorResponse(error)
-                    ? 'This page does not exist.'
-                    : 'An unexpected error occured.'}
-                <Link color={theme.colors.tertiary}><ReactRouterLink to="/">{' '}Press me to Go Home</ReactRouterLink></Link>
-            </Text>
-        </Box>
-    );
-};
+  return (
+    <Box padding='50' my='50%'>
+        <Heading mx='auto' textAlign='center'>
+            Oops!
+        </Heading>
+        <Text>
+            {isRouteErrorResponse(error) ? 'This page does not exist, please go back.': 'An unexpected error occured.'}
+        </Text>
+    </Box>
+  )
+}
 
-export default ErrorPage;
+export default ErrorPage
