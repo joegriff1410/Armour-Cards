4231e5ee3e6ab7d8f8abc588dcdb64519db7d10b
Joe Griffin <jgriffin1410@gmail.com>
2024-01-13 08:17:21 +0000

    Basic card styling added

--- view/src/pages/SignUpPage/SignUpPage.tsx
+++ view/src/pages/SignUpPage/SignUpPage.tsx
@@ -1,24 +1,27 @@
 import { Box, Button } from '@chakra-ui/react';
 import SocialLinks from '../../components/ui/SocialLinks';
-import CardTemplate from '../../components/ui/Card';
+import CardTemplate from '../../components/ui/CardTemplate';
 import theme from '../../theme';
 
 const SignUpPage = () => {
     return (
         <>
-            <CardTemplate />
-            <Button
-                bg={theme.colors.tertiary}
-                paddingX={50}
-                color={theme.colors.primary}
-                marginBottom={3}
-                fontSize={20}
-                marginX="auto"
-            >
-                Sign Up
-            </Button>
-            <Box>
-                <SocialLinks />
+            <Box marginTop="60%">
+                <CardTemplate>
+                    <Button
+                        bg={theme.colors.tertiary}
+                        paddingX={50}
+                        color={theme.colors.primary}
+                        marginBottom={3}
+                        fontSize={20}
+                        marginX="auto"
+                    >
+                        Sign Up
+                    </Button>
+                </CardTemplate>
+                <Box marginTop="65%">
+                    <SocialLinks />
+                </Box>
             </Box>
         </>
     );
