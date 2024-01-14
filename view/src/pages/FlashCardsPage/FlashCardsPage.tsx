f74027d70bd91c104cac1ca0040fff908f461537
Joe Griffin <jgriffin1410@gmail.com>
2024-01-14 16:01:49 +0000

    Static flash cards page added

--- view/src/pages/FlashCardsPage/FlashCardsPage.tsx
+++ view/src/pages/FlashCardsPage/FlashCardsPage.tsx
@@ -1,9 +1,8 @@
-import { Box, Stack } from '@chakra-ui/react';
+import { Box, Stack, VStack } from '@chakra-ui/react';
 import SocialLinks from '../../components/SocialLinks';
 import NavBar from '../../components/NavBar';
 import CardTemplate from '../../components/CardTemplate';
-
-
+import RatingButtons from '../../components/RatingButtons';
 
 const FlashCardsPage = () => {
     return (
@@ -11,10 +10,12 @@ const FlashCardsPage = () => {
             <Stack minHeight="100vh">
                 <NavBar />
                 <CardTemplate />
+                <VStack marginTop='50%'>
+                    <RatingButtons />
+                </VStack>
                 <Box marginTop="auto" marginBottom="5%">
                     <SocialLinks />
                 </Box>
-                
             </Stack>
         </>
     );
